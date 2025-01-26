0e9d1c0876108fd1a11d212d102879bc686540ac
Giacomo Castellucci <giakispeed@live.it>
2025-01-26 21:49:00 +0100

    Task_Aggancio_Sgancio
    Aggiornato Task Aggancio Sgancio

--- Simulazione_IRB_1300_matlab/Contr_Cinematico_Task_Aggancio_e_Sgancio_Utensile/IRB1300_ABB6DOF_bodytree_mesh_RUN1.m
+++ Simulazione_IRB_1300_matlab/Contr_Cinematico_Task_Aggancio_e_Sgancio_Utensile/IRB1300_ABB6DOF_bodytree_mesh_RUN1.m
@@ -103,16 +103,20 @@ addBody(Irb1300,body5,'body4')
 addBody(Irb1300,body6,'body5')
 
 % Verify that your robot was built properly by using the showdetails or show
-% function. showdetails lists all the bodies in the MATLABÂ® command window. 
+% function. showdetails lists all the bodies in the MATLAB® command window. 
 % show displays the robot with a given configuration (home by default). 
 % Calls to axis modify the axis limits and hide the axis labels.
 showdetails(Irb1300)
 
+% config=6×2 struct
+%   JointName
+%   JointPosition
 
 % homeConfiguration returns the home configuration of the robot model with
 % q vector [0]^T
 config = homeConfiguration(Irb1300);
-
+% set config as a struct and define the joint value (2).jointPosition
+% config(2).JointPosition = pi/2;
 
  show(Irb1300, config);
 % il comando seguente con 'visual','off' permette di spegnere la mesh e
@@ -124,5 +128,14 @@ T06 = getTransform(Irb1300,config,'body6','body1');
 
 clear DH;
 % nella lezione 24 in cui prepara i file per il controllo dinamico 
-%perchÃ¨ nel File RUN2_Irb120_parameters.m riutilizza nuovamente la mmatrice
+%perchè nel File RUN2_Irb120_parameters.m riutilizza nuovamente la mmatrice
 % DH
+
+%% set new configuration
+% newConfig = config;
+% newConfig(2).JointPosition = pi;
+% newConfig(4).JointPosition = pi/4;
+% newConfig(5).JointPosition = -pi/2;
+% newConfig(6).JointPosition = -pi/2;
+% 
+% show(Irb120, newConfig);
